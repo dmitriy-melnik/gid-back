081beea56adfb80e8f7767513858dc0ef5ec1b46
yuriy070920 <surzhikov.yura@yandex.ua>
2020-03-12 13:45:48 +0200

    test comment 3

--- src/main/java/com/cio/gidservice/gidservice/GidServiceApplication.java
+++ src/main/java/com/cio/gidservice/gidservice/GidServiceApplication.java
@@ -10,7 +10,4 @@ public class GidServiceApplication {
         SpringApplication.run(GidServiceApplication.class, args);
     }
 
-    /**
-     * Многострочные коментарии это лучшее создание разработчиков!
-     */
 }
