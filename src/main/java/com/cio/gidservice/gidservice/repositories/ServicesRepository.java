6a4d708cb1716d47377463fd66c1c635713f7408
yuriy070920 <surzhikov.yura@yandex.ua>
2020-03-18 16:16:36 +0200

    Made adding services and establishments

---
+++ src/main/java/com/cio/gidservice/gidservice/repositories/ServicesRepository.java
@@ -0,0 +1,12 @@
+package com.cio.gidservice.gidservice.repositories;
+
+import com.cio.gidservice.gidservice.entities.Service;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ServicesRepository extends JpaRepository<Service, Long> {
+    List<Service> findAllByEstablishment_Name(String s);
+    List<Service> findServicesByDescriptionContainingOrNameContains(String s1, String s2);
+}
+
