38c90cf73f4e85a11674a83f09bd8c16fb7eb508
Dmitriy Melnik <buddy.manlik@gmail.com>
2020-03-26 16:00:57 +0200

    commit

---
+++ src/main/java/com/cio/gidservice/gidservice/entities/User.java
@@ -0,0 +1,26 @@
+package com.cio.gidservice.gidservice.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name="usr")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    private String password;
+    private UserType userType;
+    private String name;
+
+}
+
